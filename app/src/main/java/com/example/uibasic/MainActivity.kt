b165d3afb0a6ea75e1801ac2f9573898467fe5ab
Seulbi-Lee-project <ifssfws@gmail.com>
2023-08-02 11:33:51 +0900

    연습 문제 2 버튼 추가

--- app/src/main/java/com/example/uibasic/MainActivity.kt
+++ app/src/main/java/com/example/uibasic/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.uibasic
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.text_views)
+    }
+
+    fun doActon(v:View){
+        Toast.makeText(this, "Submitted Successfully", Toast.LENGTH_SHORT).show()
     }
 }
